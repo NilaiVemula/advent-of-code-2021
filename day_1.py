c0f4ed8c9164f4f88d1fa7a293aafa91b6244892
Nilai Vemula <nilaivemula@gmail.com>
2021-12-01 00:37:11 -0500

    day 1 one-liners

---
+++ day_1.py
@@ -0,0 +1,26 @@
+def read_data():
+    with open('input/day_1.txt') as f:
+        data = f.read().splitlines()
+    data = list(map(int, data))
+
+    return data
+
+
+def part_1(data):
+    counter = sum([1 if data[i] > data[i - 1]
+                   else 0 for i in range(1, len(data))])
+    return counter
+
+
+def part_2(data):
+    counter = sum([1 if data[i + 1] > data[i - 2]
+                   else 0 for i in range(1, len(data)-1)])
+    return counter
+
+
+if __name__ == '__main__':
+    data = read_data()
+    ans_1 = part_1(data)
+    ans_2 = part_2(data)
+    print(ans_1)
+    print(ans_2)
